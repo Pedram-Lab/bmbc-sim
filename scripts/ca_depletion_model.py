3ca20c9dd48093659eecd74e27fa47fd70c63271
Michael Innerberger <innerbergerm@hhmi.org>
2024-07-25 16:53:09 -0400

    Refactor geometry creation

--- scripts/ca_depletion_model.py
+++ scripts/ca_depletion_model.py
@@ -19,31 +19,36 @@ from netgen.csg import *
 
 
 # %%
-def create_geometry(side_length, cytosol_height, ecs_height):
+def create_axis_aligned_plane(axis, offset, direction, boundary_condition=None):
+    point = Pnt(*[0 if i != axis else offset for i in range(3)])
+    normal = Vec(*[0 if i != axis else direction for i in range(3)])
+    plane = Plane(point, normal)
+    plane.bc(boundary_condition if boundary_condition is not None else "default")
+    return plane
+
+
+def create_geometry(*, side_length, cytosol_height, ecs_height):
     geometry = CSGeometry()
 
-    cytosol_left = Plane(Pnt(0, 0, 0), Vec(-1, 0, 0)).bc("cytosol_left")
-    cytosol_right = Plane(Pnt(side_length, side_length, cytosol_height), Vec(1, 0, 0))
-    cytosol_front = Plane(Pnt(0, 0, 0), Vec(0, -1, 0))
-    cytosol_back = Plane(Pnt(side_length, side_length, cytosol_height), Vec(0, 1, 0))
-    cytosol_bot = Plane(Pnt(0, 0, 0), Vec(0, 0, -1))
-    cytosol_top = Plane(Pnt(side_length, side_length, cytosol_height), Vec(0, 0, 1))
-    cytosol = cytosol_left * cytosol_right * cytosol_front * cytosol_back * cytosol_bot * cytosol_top
+    left = create_axis_aligned_plane(0, -side_length / 2, -1)
+    right = create_axis_aligned_plane(0, side_length / 2, 1)
+    front = create_axis_aligned_plane(1, -side_length / 2, -1)
+    back = create_axis_aligned_plane(1, side_length / 2, 1)
 
-    ecs_left = Plane(Pnt(0, 0, cytosol_height), Vec(-1, 0, 0))
-    ecs_right = Plane(Pnt(side_length, side_length, ecs_height), Vec(1, 0, 0))
-    ecs_front = Plane(Pnt(0, 0, cytosol_height), Vec(0, -1, 0))
-    ecs_back = Plane(Pnt(side_length, side_length, ecs_height), Vec(0, 1, 0))
-    ecs_bot = Plane(Pnt(0, 0, cytosol_height), Vec(0, 0, -1)).bc("ecs_bot")
-    ecs_top = Plane(Pnt(side_length, side_length, ecs_height), Vec(0, 0, 1))
-    ecs = ecs_left * ecs_right * ecs_front * ecs_back * ecs_bot * ecs_top
+    cytosol_bot = create_axis_aligned_plane(2, 0, -1)
+    cytosol_top = create_axis_aligned_plane(2, cytosol_height, 1)
+    cytosol = left * right * front * back * cytosol_bot * cytosol_top
+
+    ecs_bot = create_axis_aligned_plane(2, cytosol_height, -1)
+    ecs_top = create_axis_aligned_plane(2, cytosol_height + ecs_height, 1, "fixed")
+    ecs = left * right * front * back * ecs_bot * ecs_top
 
     geometry.Add(cytosol + ecs)
     return geometry
 
 
 # %%
-ngmesh = create_geometry(side_length=3, cytosol_height=3, ecs_height=3.1).GenerateMesh(maxh=0.25)
+ngmesh = create_geometry(side_length=3, cytosol_height=3, ecs_height=0.1).GenerateMesh(maxh=0.25)
 
 # %%
 mesh = Mesh(ngmesh)
@@ -52,8 +57,4 @@ mesh = Mesh(ngmesh)
 Draw(mesh)
 
 # %%
-print("Bnd = ", mesh.GetBoundaries())
-
-# %%
-mesh.ngmesh.SetBCName(0, "other_name")
 print("Bnd = ", mesh.GetBoundaries())
