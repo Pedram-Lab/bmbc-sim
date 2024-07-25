2a60a45ee18d6a41a5420820889d03ecf816b486
Michael Innerberger <innerbergerm@hhmi.org>
2024-07-25 18:39:00 -0400

    Add flux through membrane

--- scripts/ca_depletion_model.py
+++ scripts/ca_depletion_model.py
@@ -29,25 +29,30 @@ def create_geometry(*, side_length, cytosol_height, ecs_height, mesh_size):
     front = create_axis_aligned_plane(1, -side_length / 2, -1)
     back = create_axis_aligned_plane(1, side_length / 2, 1)
 
-    cutout = create_axis_aligned_cylinder(2, 0, 0, 0.1) \
-             * create_axis_aligned_plane(2, cytosol_height + ecs_height / 2, 1) \
+    cytosol_cutout = create_axis_aligned_cylinder(2, 0, 0, 0.1) \
+             * create_axis_aligned_plane(2, cytosol_height, 1, "channel") \
              * create_axis_aligned_plane(2, cytosol_height - ecs_height / 2, -1)
-    cutout.maxh(mesh_size / 2)
+    cytosol_cutout.maxh(mesh_size / 2)
 
     cytosol_bot = create_axis_aligned_plane(2, 0, -1)
     cytosol_top = create_axis_aligned_plane(2, cytosol_height, 1, "membrane")
     cytosol = left * right * front * back * cytosol_bot * cytosol_top
     cytosol.maxh(mesh_size)
 
+    ecs_cutout = create_axis_aligned_cylinder(2, 0, 0, 0.1) \
+             * create_axis_aligned_plane(2, cytosol_height + ecs_height / 2, 1) \
+             * create_axis_aligned_plane(2, cytosol_height, -1, "channel")
+    ecs_cutout.maxh(mesh_size / 2)
+    
     ecs_bot = create_axis_aligned_plane(2, cytosol_height, -1, "membrane")
     ecs_top = create_axis_aligned_plane(2, cytosol_height + ecs_height, 1, "ecs_top")
     ecs = left * right * front * back * ecs_bot * ecs_top
     ecs.maxh(mesh_size)
 
-    geometry.Add(cytosol - cutout)
-    geometry.Add(cytosol * cutout)
-    geometry.Add(ecs - cutout)
-    geometry.Add(ecs * cutout)
+    geometry.Add(cytosol - cytosol_cutout)
+    geometry.Add(cytosol * cytosol_cutout)
+    geometry.Add(ecs - ecs_cutout)
+    geometry.Add(ecs * ecs_cutout)
     return geometry
 
 
@@ -67,6 +72,7 @@ v = fes.TestFunction()
 
 f = LinearForm(fes)
 f += 0 * v * dx
+f += 1 * v.Trace() * ds(definedon="membrane")
 
 a = BilinearForm(fes)
 a += grad(u) * grad(v) * dx
