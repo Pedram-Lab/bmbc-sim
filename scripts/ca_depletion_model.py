88d36adba9a13c2877bf97f431c291a18bbebcab
Michael Innerberger <innerbergerm@hhmi.org>
2024-07-25 17:10:46 -0400

    Move general utility function to ecsim package

--- scripts/ca_depletion_model.py
+++ scripts/ca_depletion_model.py
@@ -17,16 +17,10 @@ from ngsolve import *
 from ngsolve.webgui import Draw
 from netgen.csg import *
 
+from ecsim.geometry import create_axis_aligned_plane
+
 
 # %%
-def create_axis_aligned_plane(axis, offset, direction, boundary_condition=None):
-    point = Pnt(*[0 if i != axis else offset for i in range(3)])
-    normal = Vec(*[0 if i != axis else direction for i in range(3)])
-    plane = Plane(point, normal)
-    plane.bc(boundary_condition if boundary_condition is not None else "default")
-    return plane
-
-
 def create_geometry(*, side_length, cytosol_height, ecs_height):
     geometry = CSGeometry()
 
