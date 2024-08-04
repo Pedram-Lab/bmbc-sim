d9702a57d6b38dd524380da8dc23fd99530e75d7
Michael Innerberger <innerbergerm@hhmi.org>
2024-08-04 17:59:20 -0400

    Split static and dynamic Ca-depletion model

--- scripts/ca_depletion_model_dynamic.py
+++ scripts/ca_depletion_model_dynamic.py
@@ -13,7 +13,10 @@
 # ---
 
 # %% [markdown]
-# This script takes the `geometry from scripts/channel_geometry_with_occ.py` and adds reaction-diffusion of chemical species on top and adds the library Astropy to handle units.
+# This script takes the `geometry from scripts/channel_geometry_with_occ.py` and adds reaction-diffusion of chemical species on top (units are handled by astropy):
+# * Ca can diffuse from the ECS to the cytosol through the channel.
+#
+# The dynamics of the system are resolved in time.
 
 # %%
 from ngsolve import *
@@ -47,18 +50,11 @@ i_max = 0.1 * u.picoampere
 # %%
 # Create meshed geometry
 mesh = create_ca_depletion_mesh(side_length=3, cytosol_height=3, ecs_height=0.1, mesh_size=0.25, channel_radius=0.5)
-print(mesh.GetBoundaries())
-
-# %%
-clipping = {"function": True,  "pnt": (0, 0, 1.5), "vec": (0, 1, 0)}
-settings = {"camera": {"transformations": [{"type": "rotateX", "angle": -60}]}}
-Draw(mesh, clipping=clipping, settings=settings)
 
 # %%
 # Define and assemble the FE-problem
-# We set the cytosol boundary to zero for visualization purposes
 ecs_fes = H1(mesh, order=2, definedon=mesh.Materials("ecs"), dirichlet="ecs_top")
-cytosol_fes = H1(mesh, order=2, definedon=mesh.Materials("cytosol"), dirichlet="boundary")
+cytosol_fes = H1(mesh, order=2, definedon=mesh.Materials("cytosol"))
 fes = FESpace([ecs_fes, cytosol_fes])
 u_ecs, u_cyt = fes.TrialFunction()
 v_ecs, v_cyt = fes.TestFunction()
@@ -72,20 +68,6 @@ a += (u_ecs - u_cyt) * (v_ecs - v_cyt) * ds("channel")  # interface flux
 
 a.Assemble()
 f.Assemble()
-
-# %%
-# Set concentration at top to 15 and solve the system
-concentration = GridFunction(fes)
-concentration.components[0].Set(15, definedon=mesh.Boundaries("ecs_top"))
-res = f.vec.CreateVector()
-res.data = f.vec - a.mat * concentration.vec
-concentration.vec.data += a.mat.Inverse(fes.FreeDofs()) * res
-
-# %%
-# Visualize (the colormap is quite extreme for dramatic effect)
-visualization = mesh.MaterialCF({"ecs": concentration.components[0], "cytosol": concentration.components[1]})
-settings = {"camera": {"transformations": [{"type": "rotateX", "angle": -90}]}, "Colormap": {"ncolors": 256, "autoscale": False, "max": 3}}
-Draw(visualization, mesh, clipping=clipping, settings=settings)
 
 # %%
 # Time stepping - set up system matrix
@@ -119,11 +101,12 @@ def time_stepping(u, t_end, n_samples):
 # %%
 # Time stepping - set initial conditions and do time stepping
 concentration = GridFunction(fes)
-concentration.components[0].Set(15, definedon=mesh.Boundaries("ecs_top"))
+concentration.components[0].Set(15)
 c_t = time_stepping(concentration, t_end=1, n_samples=100)
 
 # %%
-# Visualize (because of the product structure of the FESpace, the usual visualization of time-dependent functions via multidim
+# Visualize (because of the product structure of the FESpace, the usual
+# visualization of time-dependent functions via multidim is not possible)
 visualization = mesh.MaterialCF({"ecs": c_t.components[0], "cytosol": c_t.components[1]})
 settings = {"camera": {"transformations": [{"type": "rotateX", "angle": -90}]}, "Colormap": {"ncolors": 32, "autoscale": False, "max": 15}}
 Draw(c_t.components[1], clipping=clipping, settings=settings, interpolate_multidim=True, animate=True)
