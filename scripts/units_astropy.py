3fb719ac2b88db128256c5d2b725ed7e7bf7e655
Michael Innerberger <innerbergerm@hhmi.org>
2024-08-05 17:00:20 -0400

    Change minor things in units_astropy (mostly output)

--- scripts/units_astropy.py
+++ scripts/units_astropy.py
@@ -23,7 +23,6 @@
 # The module`astropy.constants` includes a wide range of physical constants. For example,   
 
 # %%
-import astropy
 from astropy import constants as const
 print(const.N_A)
 
@@ -46,19 +45,24 @@ for name in dir(const):
 # %%
 import astropy.units as u
 available_units = [attr for attr in dir(u) if isinstance(getattr(u, attr), u.UnitBase)]
+# Uncomment to print all units - warning: there are a lot of units!
+# print(available_units)
 
 # %% [markdown]
 # To define a parameter with its respective units, use the module `units` followed by the name of the unit. For example, the Faraday constant $F = 9.65 \times 10^{-11} \frac{\text{C}}{\text{fmol}}$, can be defined as:
 
 # %%
 F_femtomol = 9.65e-11 * u.C / u.femtomole
+print(f"Pretty output by default: {F_femtomol}")
+print(f"F has a value '{F_femtomol.value}' and a unit '{F_femtomol.unit}'")
 
 # %% [markdown]
 # Conversion of the units can be handle using the module `.to`. For example, converting $F = 9.65 \times 10^{-11} \frac{\text{C}}{\text{fmol}}$ to $F = 96.5 \frac{\text{C}}{\text{mmol}}$, can be code as: 
 
 # %%
-F_femtomol = 9.65e-11 * u.C / u.femtomole
 F_millimol = F_femtomol.to(u.C / u.millimole)
+print(f"Before conversion: {F_femtomol}")
+print(f"After conversion: {F_millimol}")
 
 # %% [markdown]
 # This will automatically handle the ($10^{-15}$) conversion factor inherent in the "femto-" prefix.
@@ -74,7 +78,7 @@ ca_ext = 15 * u.millimole
 ca_cyt = 0.0001 * u.millimole
 delta_ca = ca_ext - ca_cyt
 I_ch = nu * delta_ca
-print(f"I_ch = {I_ch.value}{I_ch.unit}")
+print(f"I_ch = {I_ch}")
 
 # %% [markdown]
 # Astropy generates an error message when incompatible units are used together. The code below is designed to intentionally trigger such an error due to unit incompatibility:
@@ -84,8 +88,11 @@ nu = 4 * u.picoSiemens
 ca_ext = 15 * u.millimole
 ca_cyt = 0.0001 * u.millimole
 delta_ca = ca_ext - ca_cyt
-I_ch = nu + delta_ca
-print(f"I_ch = {I_ch.value}{I_ch.unit}")
+try:
+    I_ch = nu + delta_ca
+except Exception as e:
+    print(f"The following {type(e).__name__} occurred:")
+    print(f"    \"{e}\"")
 
 # %% [markdown]
 # ## Using Numpy with Astropy Units
@@ -96,10 +103,13 @@ print(f"I_ch = {I_ch.value}{I_ch.unit}")
 import numpy as np
 
 # Create a NumPy array with Astropy units
-ca_ext, ca_cyt = np.array([15, 0.0001]) * u.millimole
+ca = np.array([15, 0.0001]) * u.millimole
+ca_ext, ca_cyt = ca
 
-print("[Ca2+]_ext:", ca_ext)
-print("[Ca2+]_cyt:", ca_cyt)
+print(f"[Ca2+]_ext: {ca_ext}")
+print(f"[Ca2+]_cyt: {ca_cyt}")
+print(f"As an array: {ca}")
+print(f"Units are also respected in Numpy operations, e.g., the dot product: {ca.dot(ca)}")
 
 # %% [markdown]
 # The following code calculates the flux for a single channel. This computation will be scrutinized thoroughly due to the unexpected result concerning the units.
@@ -114,82 +124,29 @@ F_femtomol = 9.65e-11 * u.C / u.femtomole
 F_millimol = F_femtomol.to(u.C / u.millimole)
 dv = 1.25e-19 * u.liter
 J_ch = I_ch / (2*F_millimol*dv)
-print(f"J_ch = {J_ch.value}{J_ch.unit}")
+print(f"J_ch = {J_ch}")
 
 # %% [markdown]
 # The following lines contain the parameter that will be used in the calcium depletion model.
 
 # %%
 ca_ext = 15 * u.millimole
-print(ca_ext)
-
-# %%
 ca_cyt = 0.0001 * u.millimole
-print(ca_cyt)
-
-# %%
 egta_1 = 4.5 * u.millimole
-print(egta_1)
-
-# %%
 egta_2 = 40 * u.millimole
-print(egta_2)
-
-# %%
 bapta = 1 * u.millimole
-print(bapta)
-
-# %%
 diff_ca_ext = 600 * u.um**2 / u.s
-print(diff_ca_ext)
-
-# %%
 diff_ca_cyt = 220 * u.um**2 / u.s
-print(diff_ca_cyt)
-
-# %%
 diff_free_egta = 113 * u.um**2 / u.s
-print(diff_free_egta)
-
-# %%
 diff_bound_egta = 113 * u.um**2 / u.s
-print(diff_free_egta)
-
-# %%
 diff_free_bapta = 95 * u.um**2 / u.s
-print(diff_free_bapta)
-
-# %%
 diff_bound_bapta = 113 * u.um**2 / u.s
-print(diff_free_bapta)
-
-# %%
 k_f_egta = 2.7 * u.micromole / u.s
-print(k_f_egta)
-
-# %%
 k_r_egta = 0.5 / u.s
-print(k_r_egta)
-
-# %%
 k_f_bapta = 450 * u.micromole / u.s
-print(k_f_bapta)
-
-
-# %%
 k_r_bapta = 80 / u.s
-print(k_r_bapta)
-
-# %%
 diameter_ch = 10 * u.nm
-print(diameter_ch)
-
-# %%
 density_channel = 10000 / u.um**2
-print(density_channel)
-
-# %%
 i_max = 0.1 * u.picoampere
-print(i_max)
 
 # %%
