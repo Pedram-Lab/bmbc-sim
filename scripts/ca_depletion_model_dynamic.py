33c998ed98ec7c484fa95f223b150e5572da54f7
Michael Innerberger <innerbergerm@hhmi.org>
2024-08-18 21:56:12 -0400

    Add first version of domain specific syntax

--- scripts/ca_depletion_model_dynamic.py
+++ scripts/ca_depletion_model_dynamic.py
@@ -22,27 +22,12 @@
 from ngsolve import *
 from ngsolve.webgui import Draw
 from tqdm.notebook import trange
+from astropy import units as u
 
 from ecsim.geometry import create_ca_depletion_mesh
-from astropy import units as u
-import matplotlib.pyplot as plt
+from ecsim.simulation import Simulation
 
 # %%
-ca_ext = 15 * u.millimole
-ca_cyt = 0.0001 * u.millimole
-egta_1 = 4.5 * u.millimole
-egta_2 = 40 * u.millimole
-bapta = 1 * u.millimole
-diff_ca_ext = 600 * u.um**2 / u.s
-diff_ca_cyt = 220 * u.um**2 / u.s
-diff_free_egta = 113 * u.um**2 / u.s
-diff_bound_egta = 113 * u.um**2 / u.s
-diff_free_bapta = 95 * u.um**2 / u.s
-diff_bound_bapta = 113 * u.um**2 / u.s
-k_f_egta = 2.7 * u.micromole / u.s
-k_r_egta = 0.5 / u.s
-k_f_bapta = 450 * u.micromole / u.s
-k_r_bapta = 80 / u.s
 diameter_ch = 10 * u.nm
 density_channel = 10000 / u.um**2
 i_max = 0.1 * u.picoampere
@@ -52,98 +37,57 @@ i_max = 0.1 * u.picoampere
 mesh = create_ca_depletion_mesh(side_length=3 * u.um, cytosol_height=3 * u.um, ecs_height=0.1 * u.um, mesh_size=0.25 * u.um, channel_radius=0.5 * u.um)
 
 # %%
-# Define and assemble the FE-problem
-ecs_fes = H1(mesh, order=2, definedon=mesh.Materials("ecs"), dirichlet="ecs_top")
-cytosol_fes = H1(mesh, order=2, definedon=mesh.Materials("cytosol"))
-fes = FESpace([ecs_fes, cytosol_fes])
-u_ecs, u_cyt = fes.TrialFunction()
-v_ecs, v_cyt = fes.TestFunction()
-u1_cyt, v1_cyt = cytosol_fes.TnT()
-
-calcium = GridFunction(fes)
-buffer = GridFunction(cytosol_fes)
-complexes = GridFunction(cytosol_fes)
-
-a_ca = BilinearForm(fes)
-a_ca += grad(u_ecs) * grad(v_ecs) * dx("ecs")              # diffusion in ecs
-a_ca += grad(u_cyt) * grad(v_cyt) * dx("cytosol")          # diffusion in cytosol
-a_ca += (u_ecs - u_cyt) * (v_ecs - v_cyt) * ds("channel")  # interface flux
-
-a_buffer = BilinearForm(cytosol_fes)
-a_buffer += grad(u1_cyt) * grad(v1_cyt) * dx
-
-f_ca = LinearForm(fes)
-f_ca += -buffer * calcium.components[1] * v_cyt * dx("cytosol")
-f_ca += complexes * v_cyt * dx("cytosol")
-
-f_buf = LinearForm(cytosol_fes)
-f_buf += -buffer * calcium.components[1] * v1_cyt * dx
-f_buf += complexes * v1_cyt * dx
-
-f_com = LinearForm(cytosol_fes)
-f_com += buffer * calcium.components[1] * v1_cyt * dx
-f_com += -complexes * v1_cyt * dx
-
-a_ca.Assemble()
-a_buffer.Assemble()
+# Set up a simulation on the mesh with BAPTA as a buffer
+simulation = Simulation(mesh, time_step=1 * u.ms)
+calcium = simulation.add_species("calcium", diffusivity={"ecs": 600 * u.um**2 / u.s, "cytosol": 220 * u.um**2 / u.s})
+free_buffer = simulation.add_species("free_buffer", diffusivity={"cytosol": 95 * u.um**2 / u.s})
+bound_buffer = simulation.add_species("bound_buffer", diffusivity={"cytosol": 113 * u.um**2 / u.s})
+# simulation.add_reaction(reactants=(calcium, free_buffer), products=bound_buffer, kf={"cytosol": 450 * u.micromole / u.s}, kr={"cytosol": 80 / u.s})
+simulation.add_reaction(reactants=(calcium, free_buffer), products=bound_buffer, kf={"cytosol": 1000 * u.millimole / u.s}, kr={"cytosol": 1 / u.s})
+simulation.add_channel_flux(left="ecs", right="cytosol", boundary="channel", rate=1 * u.millimole / u.s)
+    
+# Alternative: EGTA as buffer
+# free_buffer = simulation.add_species("free_buffer", diffusivity={"cytosol": 113 * u.um**2 / u.s})
+# bound_buffer = simulation.add_species("bound_buffer", diffusivity={"cytosol": 113 * u.um**2 / u.s})
+# simulation.add_reaction(reactants=(calcium, free_buffer), products=bound_buffer, kf={"cytosol": 2.7 * u.micromole / u.s}, kr={"cytosol": 0.5 / u.s})
 
 # %%
-# Time stepping - set up system matrix
-m_ca = BilinearForm(fes)
-m_ca += u_ecs * v_ecs * dx("ecs")
-m_ca += u_cyt * v_cyt * dx("cytosol")
-m_ca.Assemble()
+# Internally set up all finite element infrastructure
+simulation.setup_problem()
 
-dt = 0.001
-mstar_ca = m_ca.mat.CreateMatrix()
-mstar_ca.AsVector().data = m_ca.mat.AsVector() + dt * a_ca.mat.AsVector()
-mstar_ca_inv = mstar_ca.Inverse(freedofs=fes.FreeDofs())
-
-m_buffer = BilinearForm(cytosol_fes)
-m_buffer += u1_cyt * v1_cyt * dx
-m_buffer.Assemble()
-
-mstar_buffer = m_buffer.mat.CreateMatrix()
-mstar_buffer.AsVector().data = m_buffer.mat.AsVector() + dt * a_buffer.mat.AsVector()
-mstar_buffer_inv = mstar_buffer.Inverse(freedofs=cytosol_fes.FreeDofs())
 
 # %%
 # Time stepping - define a function that pre-computes all timesteps
-def time_stepping(u_ca, u_buf, u_com, t_end, n_samples):
-    n_steps = int(ceil(t_end.value / dt))
+def time_stepping(simulation, t_end, n_samples):
+    n_steps = int(ceil(t_end.value / simulation._time_step_size.to(u.s).value))
     sample_int = int(ceil(n_steps / n_samples))
-    u_ca_t = GridFunction(u_ca.space, multidim=0)
-    u_buf_t = GridFunction(u_buf.space, multidim=0)
-    u_com_t = GridFunction(u_com.space, multidim=0)
-    u_ca_t.AddMultiDimComponent(u_ca.vec)
-    u_buf_t.AddMultiDimComponent(u_buf.vec)
-    u_com_t.AddMultiDimComponent(u_com.vec)
+    u_ca_t = GridFunction(simulation._fes, multidim=0)
+    u_buf_t = GridFunction(simulation._fes, multidim=0)
+    u_com_t = GridFunction(simulation._fes, multidim=0)
+    u_ca_t.AddMultiDimComponent(simulation.concentrations["calcium"].vec)
+    u_buf_t.AddMultiDimComponent(simulation.concentrations["free_buffer"].vec)
+    u_com_t.AddMultiDimComponent(simulation.concentrations["bound_buffer"].vec)
     
     for i in trange(n_steps):
-        f_ca.Assemble()
-        f_buf.Assemble()
-        f_com.Assemble()
-        res_ca = dt * (f_ca.vec - a_ca.mat * u_ca.vec)
-        res_buf = dt * (f_buf.vec - a_buffer.mat * u_buf.vec)
-        res_com = dt * (f_com.vec - a_buffer.mat * u_com.vec)
-        u_ca.vec.data += mstar_ca_inv * res_ca
-        u_buf.vec.data += mstar_buffer_inv * res_buf
-        u_com.vec.data += mstar_buffer_inv * res_com
+        simulation.time_step()
         if i % sample_int == 0:
-            u_ca_t.AddMultiDimComponent(u_ca.vec)
-            u_buf_t.AddMultiDimComponent(u_buf.vec)
-            u_com_t.AddMultiDimComponent(u_com.vec)
+            u_ca_t.AddMultiDimComponent(simulation.concentrations["calcium"].vec)
+            u_buf_t.AddMultiDimComponent(simulation.concentrations["free_buffer"].vec)
+            u_com_t.AddMultiDimComponent(simulation.concentrations["bound_buffer"].vec)
     return u_ca_t, u_buf_t, u_com_t
 
 
 # %%
 # Time stepping - set initial conditions and do time stepping
 with TaskManager():
-    calcium.components[0].Set(15)
-    calcium.components[1].Set(0)
-    buffer.Set(1)
-    complexes.Set(0)
-    ca_t, buffer_t, complex_t = time_stepping(calcium, buffer, complexes, t_end=1 * u.s, n_samples=100)
+    simulation.init_concentrations(
+        calcium={"ecs": 15 * u.millimole, "cytosol": 0.1 * u.micromole},
+        free_buffer={"cytosol": 1 * u.millimole}, # bapta
+        # free_buffer={"cytosol": 4.5 * u.millimole}, # low egta
+        # free_buffer={"cytosol": 40 * u.millimole}, # high egta
+        bound_buffer={"cytosol": 0 * u.millimole}
+    )
+    ca_t, buffer_t, complex_t = time_stepping(simulation, t_end=0.1 * u.s, n_samples=100)
 
 # %%
 # Visualize (because of the product structure of the FESpace, the usual
@@ -151,8 +95,8 @@ with TaskManager():
 visualization = mesh.MaterialCF({"ecs": ca_t.components[0], "cytosol": ca_t.components[1]})
 clipping = {"function": True,  "pnt": (0, 0, 1.5), "vec": (0, 1, 0)}
 settings = {"camera": {"transformations": [{"type": "rotateX", "angle": -90}]}, "Colormap": {"ncolors": 32, "autoscale": False, "max": 15}}
-# Draw(ca_t.components[1], clipping=clipping, settings=settings, interpolate_multidim=True, animate=True)
+Draw(ca_t.components[1], clipping=clipping, settings=settings, interpolate_multidim=True, animate=True)
 # Draw(buffer_t, clipping=clipping, settings=settings, interpolate_multidim=True, animate=True)
-Draw(complex_t, clipping=clipping, settings=settings, interpolate_multidim=True, animate=True)
+# Draw(complex_t, clipping=clipping, settings=settings, interpolate_multidim=True, animate=True)
 
 # %%
---
+++ src/ecsim/simulation.py
@@ -0,0 +1,212 @@
+from dataclasses import dataclass
+from typing import Dict, Iterable
+
+import astropy.units as au
+from ngsolve import Mesh, FESpace, H1, Compress, VOL, BND, BilinearForm, LinearForm, grad, dx, ds, GridFunction, CoefficientFunction
+from pyngcore import BitArray
+
+
+@dataclass
+class ChemicalSpecies:
+    name: str
+    diffusivity: Dict[str, au.Quantity]
+    clamp: Dict[str, au.Quantity]
+    # TODO: charge, ...?
+
+    @property
+    def compartments(self):
+        return self.diffusivity.keys()
+
+@dataclass
+class Reaction:
+    reactants: Iterable[ChemicalSpecies]
+    products: Iterable[ChemicalSpecies]
+    kf: Dict[str, au.Quantity]
+    kr: Dict[str, au.Quantity]
+
+@dataclass
+class ChannelFlux:
+    left: str
+    right: str
+    boundary: str
+    rate: au.Quantity
+
+class Simulation:
+    def __init__(self, mesh: Mesh, time_step: au.Quantity, order: int = 2):
+        self.mesh = mesh
+        self._time_step_size = time_step
+        self._compartments = {name: i for i, name in enumerate(mesh.GetMaterials())}
+        self._fes = FESpace([Compress(H1(mesh, order=order, definedon=mesh.Materials(name))) for name in mesh.GetMaterials()])
+        self._species = {}
+        self._reactions = []
+        self._channels = []
+        self._diffusion_matrix = {}
+        self._time_stepping_matrix = {}
+        self._source_terms = {}
+        self.concentrations = {}
+
+    def add_species(
+            self,
+            name: str,
+            *,
+            diffusivity: Dict[str, au.Quantity],
+            clamp:Dict[str, au.Quantity] = None
+    ) -> ChemicalSpecies:
+        """
+        Add a new :class:`ChemicalSpecies` to the simulation.
+        :param name: Name of the species.
+        :param diffusivity: Diffusivity in different compartments; if not given, the species is not present in the compartment.
+        :param clamp: Clamp concentration to a certain value at the boundary.
+        :return:
+        """
+        if name in self._species:
+            raise ValueError(f"Species {name} already exists.") from None
+        for compartment in diffusivity:
+            if compartment not in self._compartments:
+                raise ValueError(f"Compartment {compartment} not found in the mesh.")
+        clamp = clamp or {}
+        for boundary in clamp:
+            if boundary not in self.mesh.GetBoundaries():
+                raise ValueError(f"Boundary {boundary} not found in the mesh.")
+
+        self._species[name] = ChemicalSpecies(name, diffusivity, clamp)
+        return self._species[name]
+
+    def add_reaction(
+            self,
+            *,
+            reactants: ChemicalSpecies | Iterable[ChemicalSpecies],
+            products: ChemicalSpecies | Iterable[ChemicalSpecies],
+            kf: Dict[str, au.Quantity],
+            kr: Dict[str, au.Quantity]
+    ):
+        """
+        Add a reaction r_1 + r_2 + ... <-> p_1 + p_2 + ... with forward and reverse rate constants.
+        :param reactants: Participating :class:`ChemicalSpecies` in the reaction.
+        :param products: Products of the reaction.
+        :param kf: Forward rate constant per compartment.
+        :param kr: Reverse rate constant per compartment.
+        """
+        if not isinstance(reactants, Iterable):
+            reactants = [reactants]
+        if not isinstance(products, Iterable):
+            products = [products]
+        reaction = Reaction(reactants, products, kf, kr)
+        # TODO: check if compartments exist and all species are present in the compartments
+        self._reactions.append(reaction)
+
+    def add_channel_flux(self, left: str, right: str, boundary: str, rate: au.Quantity):
+        """
+        Add a channel flux between two compartments.
+        :param left: Name of the left compartment.
+        :param right: Name of the right compartment.
+        :param boundary: Name of the boundary where the flux occurs.
+        :param rate: Rate of the flux.
+        """
+        if left not in self._compartments:
+            raise ValueError(f"Compartment {left} not found in the mesh.")
+        if right not in self._compartments:
+            raise ValueError(f"Compartment {right} not found in the mesh.")
+        if boundary not in self.mesh.GetBoundaries():
+            raise ValueError(f"Boundary {boundary} not found in the mesh.")
+        # TODO: check if the compartments are adjacent and the boundary is between them
+
+        self._channels.append(ChannelFlux(left, right, boundary, rate))
+
+    def setup_problem(self):
+        self.concentrations = {name: GridFunction(self._fes) for name in self._species}
+
+        for name, species in self._species.items():
+                a, m_star_inv = self._setup_matrices(species)
+                self._diffusion_matrix[name] = a
+                self._time_stepping_matrix[name] = m_star_inv
+
+        self._source_terms = {name: LinearForm(self._fes) for name in self._species}
+        for reaction in self._reactions:
+            self._add_reaction_to_source_terms(reaction)
+
+    def _setup_matrices(self, species):
+        relevant_dofs = BitArray(self._fes.ndof)
+        relevant_dofs[:] = True
+
+        for compartment in self._compartments:
+            if not compartment in species.compartments:
+                # If no diffusivity is given, the species is not present in this compartment (even if the concentration is zero)
+                self._exclude_dofs(relevant_dofs, self.mesh.Region(VOL, compartment))
+        for boundary in self.mesh.GetBoundaries():
+            if boundary in species.clamp:
+                self._exclude_dofs(relevant_dofs, self.mesh.Region(BND, boundary))
+
+        # Set up diffusion and mass matrix (set check_unused=False to avoid warnings about unused DOFs)
+        a = BilinearForm(self._fes, check_unused=False)
+        m = BilinearForm(self._fes, check_unused=False)
+        u, v = self._fes.TnT()
+        for compartment, diffusivity in species.diffusivity.items():
+            i = self._compartments[compartment]
+            D = diffusivity.to(au.um**2 / au.s).value
+            a += D * grad(u[i]) * grad(v[i]) * dx(compartment)
+            m += u[i] * v[i] * dx(compartment)
+
+        for channel in self._channels:
+            i = self._compartments[channel.left]
+            j = self._compartments[channel.right]
+            rate = channel.rate.to(au.millimole / au.s).value
+            a += rate * (u[i] - u[j]) * (v[i] - v[j]) * ds(channel.boundary)
+
+        a.Assemble()
+        m.Assemble()
+
+        dt = self._time_step_size.to(au.s).value
+        m.mat.AsVector().data += dt * a.mat.AsVector()
+
+        return a, m.mat.Inverse(relevant_dofs)
+
+    def _exclude_dofs(self, dof_array, region):
+        for el in region.Elements():
+            for dof in self._fes.GetDofNrs(el):
+                dof_array[dof] = False
+
+    def _add_reaction_to_source_terms(self, reaction):
+        _, v = self._fes.TnT()
+        for compartment in reaction.kf:
+            i = self._compartments[compartment]
+            kf = reaction.kf[compartment].to(au.millimole / au.s).value
+
+            # TODO: find a better default value (or skip if reactants / products are not present in the compartment)
+            forward_reaction = CoefficientFunction(1.0)
+            for reactant in reaction.reactants:
+                forward_reaction *= self.concentrations[reactant.name].components[i]
+            for reactant in reaction.reactants:
+                self._source_terms[reactant.name] += -kf * forward_reaction.Compile() * v[i] * dx(compartment)
+            for product in reaction.products:
+                self._source_terms[product.name] += kf * forward_reaction.Compile() * v[i] * dx(compartment)
+
+        for compartment in reaction.kr:
+            i = self._compartments[compartment]
+            kr = reaction.kr[compartment].to(1 / au.s).value
+
+            reverse_reaction = CoefficientFunction(1.0)
+            for product in reaction.products:
+                reverse_reaction *= self.concentrations[product.name].components[i]
+            for reactant in reaction.reactants:
+                self._source_terms[reactant.name] += kr * reverse_reaction.Compile() * v[i] * dx(compartment)
+            for product in reaction.products:
+                self._source_terms[product.name] += -kr * reverse_reaction.Compile() * v[i] * dx(compartment)
+
+    def time_step(self):
+        residual = {}
+        dt = self._time_step_size.to(au.s).value
+        for name, f in self._source_terms.items():
+            f.Assemble()
+            a = self._diffusion_matrix[name]
+            u = self.concentrations[name]
+            residual[name] = dt * (f.vec - a.mat * u.vec)
+        for name, u in self.concentrations.items():
+            u.vec.data += self._time_stepping_matrix[name] * residual[name]
+
+    def init_concentrations(self, **initial_concentrations):
+        for name, values in initial_concentrations.items():
+            for compartment, value in values.items():
+                i = self._compartments[compartment]
+                v = value.to(au.millimole).value
+                self.concentrations[name].components[i].Set(v)
