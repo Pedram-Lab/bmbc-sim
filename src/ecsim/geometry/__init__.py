80c82ee1e69b1bd3135e21e8bc583fcafb997fb1
Michael Innerberger <innerbergerm@hhmi.org>
2025-01-29 11:07:13 -0500

    Pull Rusakov geometry into geometry module

--- src/ecsim/geometry/__init__.py
+++ src/ecsim/geometry/__init__.py
@@ -3,6 +3,7 @@ Geometry module; contains functions for creating and working with geometries for
 simulations.
 """
 from .simple_geometries import create_ca_depletion_mesh
+from .rusakov_geometry import create_rusakov_geometry
 from .evaluators import LineEvaluator, PointEvaluator
 
-__all__ = ["create_ca_depletion_mesh", "LineEvaluator", "PointEvaluator"]
+__all__ = ["create_ca_depletion_mesh", "create_rusakov_geometry", "LineEvaluator", "PointEvaluator"]
