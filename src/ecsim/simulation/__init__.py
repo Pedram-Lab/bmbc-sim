2bec184e02a64521e81846466d482e8cbfa1bfaf
Michael Innerberger <innerbergerm@hhmi.org>
2025-03-07 18:53:27 -0500

    Expose new simulation classes

--- src/ecsim/simulation/__init__.py
+++ src/ecsim/simulation/__init__.py
@@ -1,6 +1,8 @@
 from .simulation import Simulation
 from .simulation_agents import ChemicalSpecies, Reaction, ChannelFlux
 from .utils import SimulationClock
+from .geometry_description import GeometryDescription, Membrane, Compartment, Region
 
 
-__all__ = ["Simulation", "ChemicalSpecies", "Reaction", "ChannelFlux", "SimulationClock"]
+__all__ = ["Simulation", "ChemicalSpecies", "Reaction", "ChannelFlux",
+           "SimulationClock", "GeometryDescription", "Membrane", "Compartment", "Region"]
