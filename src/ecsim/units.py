c1a5b33d4ec25c9df63b2ffb1c4c443fda64678c
Michael Innerberger <innerbergerm@hhmi.org>
2025-03-07 13:50:44 -0500

    Change implementation of unit conversions

--- src/ecsim/units.py
+++ src/ecsim/units.py
@@ -2,56 +2,47 @@
 Defines the SI-base units used in the simulations. The choice of units is based
 on the typical values of the quantities in the simulation (e.g., concentration
 in mM, length in um, etc.).
+The bases units are:
+- length: micrometers (um)
+- time: milliseconds (ms)
+- mass: nanograms (ng)
+- amount of substance: attomoles (amol)
+- electrical current: picoamperes (pA)
+- temperature: Kelvin (K)
+- luminous intensity: candela (cd)
+- angle: radians (rad)
 """
 import astropy.units as u
 
 
-# Define base units
-MASS: u.Unit = u.ng
-TIME: u.Unit = u.ms
-LENGTH: u.Unit = u.um
-CURRENT: u.Unit = u.pA
-SUBSTANCE: u.Unit = u.amol
-TEMPERATURE: u.Unit = u.K
-LUMINOUS_INTENSITY: u.Unit = u.cd
-ANGLE: u.Unit = u.rad
+# Base units for all simulations
+# These are all SI units, but with different prefixes. They are chosen such
+# that, e.g., concentration is measured in millimolar (mM), length is measured
+# in micrometers (um), etc.
+BASES = {
+    'length': u.um,
+    'time': u.ms,
+    'mass': u.ng,
+    'amount of substance': u.amol,
+    'electrical current': u.pA,
+    'temperature': u.K,
+    'luminous intensity': u.cd,
+    'angle': u.rad
+}
 
-# Define derived units
-AREA = LENGTH ** 2
-VOLUME = LENGTH ** 3
-
-DIFFUSIVITY = AREA / TIME
-CONCENTRATION = SUBSTANCE / VOLUME
-FORWARD_RATE = (CONCENTRATION * TIME) ** (-1)
-REVERSE_RATE = TIME ** (-1)
-FLUX_RATE = LENGTH / TIME
-
-FORCE = MASS * LENGTH / TIME ** 2
-PRESSURE = FORCE / LENGTH ** 2
-
-CHARGE = CURRENT * TIME
-ENERGY = FORCE * LENGTH
-POTENTIAL = ENERGY / CHARGE
-CAPACITANCE = CHARGE / POTENTIAL
-PERMITTIVITY = CAPACITANCE / LENGTH
-
-
-def convert(
-        quantity: u.Quantity,
-        unit: u.UnitBase
-) -> float:
+def to_simulation_units(value: u.Quantity, physical_name: str = None) -> float:
+    """Convert a value to simulation units.
+    :param value: The value to convert.
+    :param physical_name: The physical quantity to convert. If not None, the
+        value is checked to be of the correct physical quantity.
     """
-    Convert a quantity to a different unit.
-    :param quantity: The quantity to convert.
-    :param unit: The unit to convert to.
-    :return: The raw value of the converted quantity.
-    :raises ValueError: If the conversion is not possible.
-    """
-    try:
-        value = quantity.to(unit).value
-    except u.UnitConversionError:
-        raise ValueError(f"Cannot convert {quantity.unit} to {unit}") from None
-    except AttributeError:
-        raise ValueError(f"Cannot convert {quantity} to {unit}") from None
+    # Check if the value is of the correct physical quantity
+    if physical_name is not None:
+        actual_names = str(u.get_physical_type(value)).split("/")
+        if physical_name not in actual_names:
+            raise ValueError(
+                f"Actual physical quantity is {actual_names}, but expected {physical_name}.")
 
-    return value
+    # Convert the value to simulation units
+    conversion_factor = value.unit.decompose(bases=BASES.values()).scale
+    return float(value.value * conversion_factor)
