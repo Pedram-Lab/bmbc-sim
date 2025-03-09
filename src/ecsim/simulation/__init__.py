b7abd426fed367b83a03201ad477bb698f624fe7
Michael Innerberger <innerbergerm@hhmi.org>
2025-03-08 20:20:31 -0500

    Infer geometry description from mesh and visualize it

--- src/ecsim/simulation/__init__.py
+++ src/ecsim/simulation/__init__.py
@@ -1,8 +1,8 @@
 from .simulation import Simulation
 from .simulation_agents import ChemicalSpecies, Reaction, ChannelFlux
 from .utils import SimulationClock
-from .geometry_description import GeometryDescription, Membrane, Compartment, Region
+from .geometry_description import GeometryDescription
 
 
 __all__ = ["Simulation", "ChemicalSpecies", "Reaction", "ChannelFlux",
-           "SimulationClock", "GeometryDescription", "Membrane", "Compartment", "Region"]
+           "SimulationClock", "GeometryDescription"]
--- src/ecsim/simulation/geometry_description.py
+++ src/ecsim/simulation/geometry_description.py
@@ -1,169 +1,109 @@
-from typing import Iterable
+from itertools import accumulate
+
+import networkx as nx
+import matplotlib.pyplot as plt
 import ngsolve as ngs
-import astropy.units as u
-
-from ecsim.units import to_simulation_units
-
-
-class Region():
-    """A region within a geometry representing a biological structure. Chemical
-    solutions are continuous across region boundaries which are not
-    :class:`Membrane`s.
-    """
-    def __init__(
-        self,
-        name: str,
-        diffusion_coefficient: u.Quantity,
-    ) -> None:
-        """Create a new region.
-        Args:
-            name: The name of the region.
-        """
-        self.name = name
-        self.diffusion_coefficient = diffusion_coefficient
-
-
-class Compartment():
-    """A compartment within a geometry representing a biological structure.
-    Chemical solutions are discontinuous across compartment boundaries. Those
-    boundaries have to be modeled explicitly as :class:`Membrane`s).
-    """
-    def __init__(
-        self,
-        name: str,
-        regions: Region | Iterable[Region]
-    ) -> None:
-        """Create a new compartment.
-
-        Args:
-            name: The name of the compartment.
-            regions: The regions within the compartment.
-        """
-        self.name = name
-        if isinstance(regions, Region):
-            self.regions = [regions]
-        else:
-            self.regions = regions
-
-    def get_regions(self) -> list[Region]:
-        """Get the regions within the compartment.
-
-        Returns:
-            The regions within the compartment.
-        """
-        return self.regions
-
-    def add_diffusion_to_form(
-        self,
-        form: ngs.BilinearForm,
-        test_fun: ngs.comp.ProxyFunction,
-        trial_fun: ngs.comp.ProxyFunction
-    ) -> None:
-        """Add the ion kinetics within the compartment to the given bilinear
-        form.
-
-        Args:
-            form: The form to describe diffusion within the compartment (changed
-                in place).
-            test_fun: A test function for the compartment.
-            trial_fun: A trial function for the compartment
-        """
-        if len(self.regions) == 1:
-            diffusion_coefficient = to_simulation_units(
-                self.regions[0].diffusion_coefficient(), 'diffusivity'
-            )
-        else:
-            mesh = form.space.mesh
-            coefficients = {
-                r.name: to_simulation_units(r.diffusion_coefficient(), 'diffusivity')
-                for r in self.region
-            }
-            diffusion_coefficient = mesh.MaterialCF(coefficients)
-
-        form += diffusion_coefficient * ngs.grad(test_fun) * ngs.grad(trial_fun) * ngs.dx
-
-
-class Membrane():
-    """A membrane within a geometry representing a biological structure. A
-    membrane separates two :class:`Compartment`s. Transport across the membrane
-    is modeled explicitly.
-    """
-    def __init__(
-        self,
-        name: str,
-        left: Compartment,
-        right: Compartment
-    ) -> None:
-        """Create a new membrane.
-
-        Args:
-            name: The name of the membrane.
-            left: The compartment on the left side of the membrane.
-            right: The compartment on the right side of the membrane.
-        """
-        self.name = name
-        self.left = left
-        self.right = right
 
 
 class GeometryDescription:
-    """A bio-chemical description of the geometry of a simulation.
+    """TODO
     """
-    def __init__(
-        self,
-        compartments: list[Compartment],
-        membranes: list[Membrane],
-    ) -> None:
+    def __init__(self, mesh: ngs.Mesh) -> None:
         """Create a new geometry description.
         Args:
-            compartments: The compartments within the geometry.
-            membranes: The membranes within the geometry.
+            mesh: The mesh to create the geometry description from.
         """
-        self.compartments = compartments
-        self.membranes = membranes
+        self.mesh = mesh
 
-
-    def validate(self, mesh: ngs.Mesh) -> None:
-        """Validate the geometry description.
-        Args:
-            mesh: The mesh to validate the geometry description against.
-        Raises:
-            ValueError: If the geometry description is invalid.
-        """
-        expected = GeometryDescription.describe_mesh(mesh)
-        actual_regions = set(r.name for c in self.compartments for r in c.get_regions())
-        actual_membranes = set(m.name for m in self.membranes)
-
-        _check_if_double_or_missing(
-            actual_regions, expected["domains"], "compartments"
-        )
-        _check_if_double_or_missing(
-            actual_membranes, expected["interfaces"], "membranes"
-        )
-
-
-    @staticmethod
-    def describe_mesh(mesh: ngs.Mesh) -> dict[str, list[str]]:
-        """Return a list of all domains and interfaces in the mesh.
-        Args:
-            mesh: The mesh to describe.
-        Returns:
-            description: A dictionary with two keys: "domains" and "interfaces".
-        """
-        return {
-            "domains": list(mesh.GetMaterials()),
-            "interfaces": list(mesh.GetBoundaries())
+        # Add all other compartments as nodes (with special node for 'outside of
+        # computational domain')
+        regions = {
+            region: name
+            for name in set(mesh.GetMaterials())
+            for region in mesh.Materials(name).Split()
         }
+        self.regions: tuple[str, ...] = tuple(set(regions.values())) + ('no_domain',)
+
+        # Cluster regions into compartments
+        compartments = set(_extract_compartment(region) for region in self.regions)
+        self.compartments: tuple[str, ...] = tuple(compartments)
+
+        # Store all edges as (left, right, name)
+        boundaries = {
+            bnd: name
+            for name in set(mesh.GetBoundaries())
+            for bnd in mesh.Boundaries(name).Split()
+        }
+        self._membrane_connectivity = set()
+        for bnd, name in boundaries.items():
+            neighbors = bnd.Neighbours(ngs.VOL).Split()
+
+            n1 = regions[neighbors[0]]
+            n2 = regions[neighbors[1]] if len(neighbors) > 1 else 'no_domain'
+
+            self._membrane_connectivity.add((n1, n2, name))
+
+        # Store membrane names
+        membranes = set(name for _, _, name in self._membrane_connectivity)
+        self.membranes: tuple[str, ...] = tuple(membranes)
 
 
-def _check_if_double_or_missing(
-        entities_to_check: list[Compartment | Membrane],
-        expected_names: list[str],
-        entity_name: str
-    ) -> None:
-    double_names = list(name for name in entities_to_check if entities_to_check.count(name) > 1)
-    if double_names:
-        raise ValueError(f"Duplicate {entity_name}: {double_names}")
-    missing_names = set(e.name for e in entities_to_check) - set(expected_names)
-    if missing_names:
-        raise ValueError(f"Missing {entity_name}: {missing_names}")
+    def visualize(self, resolve_regions: bool = True) -> None:
+        """Visualize the geometry description. The compartments are
+        represented as nodes and the membranes as edges.
+        Args:
+            resolve_regions: Whether to resolve regions or not. If True, the
+                regions are used as nodes. If False, the compartments are used
+                as nodes.
+        """
+        # Create a graph with compartments as nodes and membranes as edges
+        graph = nx.MultiGraph()
+        if resolve_regions:
+            graph.add_nodes_from(self.regions)
+            for left, right, name in self._membrane_connectivity:
+                graph.add_edge(left, right, name=name)
+        else:
+            graph.add_nodes_from(self.compartments)
+            for left, right, name in self._membrane_connectivity:
+                graph.add_edge(
+                    _extract_compartment(left),
+                    _extract_compartment(right),
+                    name=name,
+                )
+
+        # Draw the graph
+        pos = nx.spring_layout(graph)
+        nx.draw_networkx_nodes(graph, pos=pos, node_color='white')
+        nx.draw_networkx_labels(graph, pos=pos)
+
+        # Draw lables for (potentially multiple!) edges
+        multiplicity = max(count for _, _, count in graph.edges(keys=True)) + 1
+        connectionstyle = [f"arc3,rad={r}" for r in accumulate([0.15] * multiplicity)]
+        nx.draw_networkx_edges(
+            graph,
+            pos=pos,
+            connectionstyle=connectionstyle,
+            edge_color="lightgrey"
+        )
+        labels = {
+            tuple(edge): attrs['name']
+            for *edge, attrs in graph.edges(keys=True, data=True)
+        }
+        nx.draw_networkx_edge_labels(
+            graph,
+            pos=pos,
+            connectionstyle=connectionstyle,
+            edge_labels=labels,
+            bbox={"alpha": 0},
+        )
+        plt.show()
+
+
+def _extract_compartment(region: str) -> str:
+    """Extract the compartment name from a region name.
+    Args:
+        region: The region name.
+    Returns:
+        The compartment name.
+    """
+    return region.split(':')[0] if ':' in region else region
