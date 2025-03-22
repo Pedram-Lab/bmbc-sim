247c816dda3ef1f681577e370f99e45040a59ebf
Michael Innerberger <innerbergerm@hhmi.org>
2025-03-22 19:53:17 -0400

    Add recorder for compartment-wise substances

--- test_new_simulation.py
+++ test_new_simulation.py
@@ -4,6 +4,7 @@ from ngsolve.webgui import Draw
 import astropy.units as u
 
 import ecsim
+from ecsim.evaluation.total_substance_recorder import CompartmentSubstance
 from ecsim.evaluation.vtk_recorder import FullSnapshot
 
 
@@ -29,6 +30,7 @@ cell = geometry.compartments['cell']
 ca = simulation.add_species('Ca', valence=2)
 
 simulation.add_recorder(FullSnapshot(100 * u.us))
+simulation.add_recorder(CompartmentSubstance(100 * u.us))
 
 cell.initialize_species(ca, value=0.5 * u.mmol / u.L)
 cell.add_diffusion(
