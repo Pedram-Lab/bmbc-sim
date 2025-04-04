8dca15655d3a2a9159b78269534e07db5f2ad27b
nprzrosas <nprzrosas@gmail.com>
2025-04-04 13:30:20 -0400

    Add species, initial conditions, reactions to Tsien simulation

--- scripts/tsien_simulation.py
+++ scripts/tsien_simulation.py
@@ -3,6 +3,7 @@ import astropy.units as u
 
 import ecsim
 from ecsim.geometry import create_ca_depletion_mesh
+from ecsim.simulation import recorder, transport
 
 
 # Dimensions
@@ -37,6 +38,125 @@ print(f"cytosol volume: {cytosol.volume}")
 print(f"Channel area: {channel.area}")
 
 # Add species to the simulation
+ca_ecs = simulation.add_species('Ca_ECS', valence=2)
+ca_cyt = simulation.add_species('Ca_cyt', valence=2)
+
+egta = simulation.add_species('EGTA', valence=-2)
+ca_egta = simulation.add_species('Ca_EGTA', valence=0)
+
+bapta = simulation.add_species('BAPTA', valence=-2)
+ca_bapta = simulation.add_species('Ca_BAPTA', valence=0)
+
+# Initial conditions
+ca_ecs_0 = 15 * u.mmol / u.L
+ca_cyt_0 = 0.1 * u.umol / u.L
+
+bapta_0 = 1 * u.mmol / u.L
+ca_bapta_0 = 0 * u.mmol / u.L
+
+# egta_low_0 = 4.5 * u.mmol / u.L
+# ca_egta_low_0 = 0 * u.mmol / u.L
+
+# egta_high_0 = 40 * u.mmol / u.L
+# ca_egta_high_0 = 0 * u.mmol / u.L
+
+ecs.initialize_species(ca_ecs, value=ca_ecs_0)
+cytosol.initialize_species(ca_cyt, value=ca_cyt_0)
+
+cytosol.initialize_species(bapta, value=bapta_0)
+cytosol.initialize_species(ca_bapta, value=ca_bapta_0)
+
+# cytosol.initialize_species(egta, value=egta_low_0)
+# cytosol.initialize_species(ca_egta, value=ca_egta_low_0)
+
+# cytosol.initialize_species(egta, value=egta_high_0)
+# cytosol.initialize_species(ca_egta, value=ca_egta_high_0)
+
+# Add diffusion to the species 
+ecs.add_diffusion(species=ca_ecs, diffusivity=600 * u.um**2 / u.s)
+cytosol.add_diffusion(species=ca_cyt, diffusivity=220 * u.um**2 / u.s)
+
+cytosol.add_diffusion(species=bapta, diffusivity=95 * u.um**2 / u.s)
+cytosol.add_diffusion(species=ca_bapta, diffusivity=95 * u.um**2 / u.s)
+
+# cytosol.add_diffusion(species=egta, diffusivity =113 * u.um**2 / u.s)
+# cytosol.add_diffusion(species=ca_bapta, diffusivity =113 * u.um**2 / u.s)
+
+#Add forward and reverse reaction rates for the buffers
+kf_bapta = 450 * u.umol / (u.L * u.s)
+kr_bapta = 80 / u.s
+
+kf_egta = 2.7 * u.umol / (u.L * u.s)
+kr_egta = 0.5 / u.s
+
+# Add reaction
+cytosol.add_reaction(reactants=[ca_cyt, bapta], products=[ca_bapta], k_f=kf_bapta, k_r=kr_bapta)
+# cytosol.add_reaction(reactants=[ca_cyt, egta], products=[ca_egta], k_f=kf_egta, k_r=kr_egta)
+
+rate_channel = 1 * u.mmol / (u.L * u.s) # Total flux 
+
+print(f"rate_channel: {rate_channel}")
+
+channel.add_transport(
+    species=ca_ecs, 
+    transport=transport.Channel(rate_channel),
+    source=ecs,
+    target=cytosol
+)
+
+
+# rate_channel = 1 * u.mmol / (u.L * u.s)
+
+#rate_channel = (1 * u.mmol / (u.L * u.s)).to(u.umol / (u.um**3 * u.s))
+
+# rate_channel = ((1 * u.mmol / (u.L * u.s)) * (1 * u.um)).to(u.umol / (u.um**2 * u.s))
+# print(f"rate_channel = {rate_channel}")
+
+# # Flujo original en mmol / (L·s)
+# flux_concentration = 1 * u.mmol / (u.L * u.s)
+
+# # Convierte a umol / (um³·s)
+# flux_volumetric = flux_concentration.to(u.umol / (u.um**3 * u.s))
+
+# # Supongamos que el canal tiene un "espesor" de 1 um, para pasarlo a flujo superficial
+# flux_thickness = 1 * u.um  # equivalente a volumen/área
+
+# # Ahora sí, flujo por unidad de área
+# rate_channel = (flux_volumetric * flux_thickness).to(u.umol / (u.um**2 * u.s))
+
+# print(f"rate_channel: {rate_channel}")
+
+# channel.add_transport(
+#     species=ca_ecs, 
+#     transport=transport.Channel(rate_channel),
+#     source=ecs,
+#     target=cytosol
+# )
+
+
+# # Define el flujo total
+# flux_total = (1 * u.mmol / (u.L * u.s)).to(u.umol / (u.s))  # umol/s
+
+# # Área del canal
+# channel_radius = 50 * u.nm
+# channel_area = (u.pi * channel_radius**2).to(u.um**2)
+
+# # Flujo por unidad de área (lo que espera Channel)
+# rate_channel = (flux_total / channel_area).to(u.umol / (u.um**2 * u.s))
+# print(f"rate_channel: {rate_channel}")
+
+# channel.add_transport(
+#     species=ca_ecs, 
+#     transport=transport.
+#     Channel(rate_channel),
+#     source=ecs,
+#     target=cytosol
+# )
+
+# rate=1 * u.mmol / (u.L * u.s)
+
+# Add transpor coefficients
+
 # ...
 # Copy the structure from sala_simulation
 # but with the logic of ca_depletion_model_dynamic
