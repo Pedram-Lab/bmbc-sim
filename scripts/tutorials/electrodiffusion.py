106c759a357201e470ac0f8e1ddaaaface75f44c
Michael Innerberger <innerbergerm@hhmi.org>
2025-04-22 11:38:58 -0400

    Update electrodiffusion tutorial

--- scripts/tutorials/electrodiffusion.py
+++ scripts/tutorials/electrodiffusion.py
@@ -1,81 +1,67 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # Benchmarks for electro-diffusion
-# This script aims to provide a simple benchmark for electro-diffusion in a 3D geometry by decoupling electrostatics and
-# diffusion.
+# This script aims to provide a simple benchmark for electro-diffusion in a 3D
+# geometry by decoupling electrostatics and diffusion.
 
 # %%
 from math import ceil, pi
 
 import numpy as np
 from scipy.special import erfc
-from netgen.occ import *
-from ngsolve import *
+from netgen import occ
+import ngsolve as ngs
 from ngsolve.webgui import Draw
 import matplotlib.pyplot as plt
 import astropy.units as au
 from astropy import constants as const
 from tqdm.notebook import trange
 
-from ecsim.units import *
+from ecsim.units import to_simulation_units, uM
 
 # %%
 # Parameters (of calcium in extracellular space)
-# Caution is needed when converting units: we want the geometry in um and concentrations in mM (= mmol / L = amol/um^3)
-diffusivity = convert(1 * au.um ** 2 / au.ms, DIFFUSIVITY)
-relative_permittivity = 80.0
-permittivity = convert(relative_permittivity * const.eps0, PERMITTIVITY)
+diffusivity = to_simulation_units(1 * au.um ** 2 / au.ms, 'diffusivity')
+REL_PERMITTIVITY = 80.0
+permittivity = to_simulation_units(REL_PERMITTIVITY * const.eps0, 'permittivity')
 
-F = convert(96485.3365 * au.C / au.mol, CHARGE / SUBSTANCE)
-valence = 2
-beta = convert(valence * const.e.si / (const.k_B * 310 * au.K), CHARGE / ENERGY)
+F = to_simulation_units(96485.3365 * au.C / au.mol)
+VALENCE = 2
+beta = to_simulation_units(VALENCE * const.e.si / (const.k_B * 310 * au.K))
 
-ca_ecs = convert(1e-3 * au.mmol / au.L, CONCENTRATION)
-tau = convert(1 * au.us, TIME)
-t_end = convert(1 * au.ms, TIME)
+ca_ecs = to_simulation_units(1 * uM, 'molar concentration')
+tau = to_simulation_units(1 * au.us, 'time')
+t_end = to_simulation_units(1 * au.ms, 'time')
 
 # %%
 # Define geometry
 s = 0.05
-ecs_left = Box(Pnt(-1, -s, -s), Pnt(0, s, s)).mat("left").bc("side")
-ecs_right = Box(Pnt(0, -s, -s), Pnt(1, s, s)).mat("right").bc("side")
+ecs_left = occ.Box((-1, -s, -s), (0, s, s)).mat("left").bc("side")
+ecs_right = occ.Box((0, -s, -s), (1, s, s)).mat("right").bc("side")
 
-geo = OCCGeometry(Glue([ecs_left, ecs_right]))
-mesh = Mesh(geo.GenerateMesh(maxh=0.02))
+geo = occ.OCCGeometry(occ.Glue([ecs_left, ecs_right]))
+mesh = ngs.Mesh(geo.GenerateMesh(maxh=0.02))
 Draw(mesh)
 
 
 # %%
 # Define FE spaces
-concentration_fes = H1(mesh, order=1)
-constraint_fes = FESpace("number", mesh)
-potential_fes = FESpace([concentration_fes, constraint_fes])
-concentration = GridFunction(concentration_fes)
-potential = GridFunction(potential_fes)
+concentration_fes = ngs.H1(mesh, order=1)
+constraint_fes = ngs.FESpace("number", mesh)
+potential_fes = ngs.FESpace([concentration_fes, constraint_fes])
+concentration = ngs.GridFunction(concentration_fes)
+potential = ngs.GridFunction(potential_fes)
 
 # %%
 # Define diffusion problem
 u_ecs, v_ecs = concentration_fes.TnT()
-a_ecs = BilinearForm(concentration_fes)
-a_ecs += diffusivity * grad(u_ecs) * grad(v_ecs) * dx
-m_ecs = BilinearForm(concentration_fes)
-m_ecs += u_ecs * v_ecs * dx
+a_ecs = ngs.BilinearForm(concentration_fes)
+a_ecs += diffusivity * ngs.grad(u_ecs) * ngs.grad(v_ecs) * ngs.dx
+m_ecs = ngs.BilinearForm(concentration_fes)
+m_ecs += u_ecs * v_ecs * ngs.dx
 
-f_ecs = LinearForm(concentration_fes)
-f_ecs += -diffusivity * beta * concentration * InnerProduct(grad(potential.components[0]), grad(v_ecs)) * dx
+f_ecs = ngs.LinearForm(concentration_fes)
+f_ecs += -diffusivity * beta * concentration * \
+    ngs.InnerProduct(ngs.grad(potential.components[0]), ngs.grad(v_ecs)) * ngs.dx
 
 a_ecs.Assemble()
 m_ecs.Assemble()
@@ -85,13 +71,13 @@ mstar_inv = m_ecs.mat.Inverse(concentration_fes.FreeDofs())
 # %%
 # Define problem for electrostatic potential
 (u_ecs, p), (v_ecs, q) = potential_fes.TnT()
-a_pot = BilinearForm(potential_fes)
-a_pot += permittivity * grad(u_ecs) * grad(v_ecs) * dx
-a_pot += p * v_ecs * dx
-a_pot += q * u_ecs * dx
+a_pot = ngs.BilinearForm(potential_fes)
+a_pot += permittivity * ngs.grad(u_ecs) * ngs.grad(v_ecs) * ngs.dx
+a_pot += p * v_ecs * ngs.dx
+a_pot += q * u_ecs * ngs.dx
 
-f_pot = LinearForm(potential_fes)
-f_pot += F * valence * concentration * v_ecs * dx
+f_pot = ngs.LinearForm(potential_fes)
+f_pot += F * VALENCE * concentration * v_ecs * ngs.dx
 
 a_pot.Assemble()
 a_pot_inv = a_pot.mat.Inverse(potential_fes.FreeDofs())
@@ -100,8 +86,8 @@ a_pot_inv = a_pot.mat.Inverse(potential_fes.FreeDofs())
 # Time stepping
 def time_stepping(ca, pot, t_end, tau, use_pot):
     n_steps = int(ceil(t_end / tau))
-    pot_start = GridFunction(concentration_fes)
-    ca_end = GridFunction(concentration_fes)
+    pot_start = ngs.GridFunction(concentration_fes)
+    ca_end = ngs.GridFunction(concentration_fes)
 
     initial_ca = concentration_fes.mesh.MaterialCF({"left": ca_ecs, "right": 0.0})
     ca.Set(initial_ca)
@@ -140,11 +126,9 @@ def evaluate_solution(sol, n_samples):
 
 # %%
 # Compute time evolution with the potential
-SetNumThreads(8)
-with TaskManager():
-    concentration.Set(0)
-    potential.components[0].Set(0)
-    ca_end, potential_start = time_stepping(concentration, potential, t_end=t_end, tau=tau, use_pot=True)
+concentration.Set(0)
+potential.components[0].Set(0)
+ca_end, potential_start = time_stepping(concentration, potential, t_end=t_end, tau=tau, use_pot=True)
 
 n_samples = 51
 ca_end_with, xs = evaluate_solution(ca_end, n_samples)
@@ -157,11 +141,9 @@ potential_start_with, _ = evaluate_solution(potential_start, n_samples)
 
 # %%
 # Compute time evolution without the potential
-SetNumThreads(8)
-with TaskManager():
-    concentration.Set(0)
-    potential.components[0].Set(0)
-    ca_end, potential_start = time_stepping(concentration, potential, t_end=t_end, tau=tau, use_pot=False)
+concentration.Set(0)
+potential.components[0].Set(0)
+ca_end, potential_start = time_stepping(concentration, potential, t_end=t_end, tau=tau, use_pot=False)
 
 ca_end_without, _ = evaluate_solution(ca_end, n_samples)
 potential_start_without, _ = evaluate_solution(potential_start, n_samples)
@@ -188,7 +170,7 @@ plt.show()
 # In order to compute the theoretical potential at the beginning, we need to compute the charge density
 initial_ca = concentration_fes.mesh.MaterialCF({"left": 1.0, "right": 0.0})
 concentration.Set(initial_ca)
-charge_density = F * ca_ecs * valence
+charge_density = F * ca_ecs * VALENCE
 source_strength = charge_density / permittivity
 # Draw(concentration)
 
