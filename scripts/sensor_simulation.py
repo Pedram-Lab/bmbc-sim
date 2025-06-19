d85b8c131dcbe1804473e43706d9a1f106b478da
Michael Innerberger <innerbergerm@hhmi.org>
2025-06-19 16:35:07 -0400

    Make sensor simulation callable from cli

--- scripts/sensor_simulation.py
+++ scripts/sensor_simulation.py
@@ -1,3 +1,5 @@
+import argparse
+
 import astropy.units as u
 
 import ecsim
@@ -5,84 +7,106 @@ from ecsim.geometry import create_sensor_geometry
 from ecsim.units import mM, uM, nM
 
 
-### Simulation Parameters
-SENSOR_ACTIVE = False  # Switch if sensor actively bind Ca or not
-SENSOR_LEFT = True  # Switch if sensor is present in the left compartment or the right
+def run_sensor_simulation(
+    sensor_active=False,
+    sensor_left=True,
+    side_length=200 * u.um,
+    compartment_ratio=0.5,
+    sphere_position_x=None,
+    sphere_radius=30 * u.um,
+    mesh_size=5 * u.um,
+    ca_init=0.05 * uM,
+    buffer_init=600 * uM,
+    buffer_kd=400 * nM,
+    kf_b=50 / (mM * u.s),
+    sensor_init=100 * uM,
+    sensor_kd=420 * nM,
+    kf_s=100 / (uM * u.s),
+):
+    """Run a simulation of a cube where one side is occupied by a buffer, and
+    there is a spherical region with a sensor. The sensor region can be either
+    left or right.
+    Default values are taken from [Sala, Hernández-Cruz; 1990].
+    """
+    if sphere_position_x is None:
+        sphere_position_x = 50 * u.um if sensor_left else 150 * u.um
 
-# Geometry parameters
-SIDE_LENGTH = 200 * u.um  # 200x200x200 µm cube
-COMPARTMENT_RATIO = 0.5   # Divide the cube in half along the x axis
-SPHERE_POSITION_X = 50 * u.um if SENSOR_LEFT else 150 * u.um  # Centered in one of the compartments
-SPHERE_RADIUS = 30 * u.um      # Sphere radius
-MESH_SIZE = 5 * u.um          # Mesh size
+    kr_b = kf_b * buffer_kd  # Reverse rate
+    kr_s = kf_s * sensor_kd  # Reverse rate
 
-# Calcium parameters
-CA_INIT = 1 * mM
+    # Create the mesh
+    mesh = create_sensor_geometry(
+        side_length=side_length,
+        compartment_ratio=compartment_ratio,
+        sphere_position_x=sphere_position_x,
+        sphere_radius=sphere_radius,
+        mesh_size=mesh_size
+    )
 
-# Buffer parameters
-BUFFER_INIT = 600 * uM  # Total buffer
-BUFFER_KD = 400 * nM  # Dissociation constant
-KF_B = 50 / (mM * u.s)  # Forward rate
-KR_B = KF_B * BUFFER_KD  # Reverse rate
+    # Create simulation
+    simulation = ecsim.Simulation('sensor', mesh, result_root='results')
 
-# Sensor parameters
-SENSOR_INIT = 100 * uM  # Total sensor
-SENSOR_KD = 420 * nM  # Dissociation constant
-KF_S = 100 / (uM * u.s)  # Forward rate
-KR_S = KF_S * SENSOR_KD  # Reverse rate
+    # Compartments
+    cube = simulation.simulation_geometry.compartments['cube']
 
+    # Add species - Ca
+    ca = simulation.add_species("ca", valence=2)
+    cube.initialize_species(ca, ca_init)
+    cube.add_diffusion(ca, 600 * u.um**2 / u.s)
 
-### Simulation setup
-# Create the mesh
-mesh = create_sensor_geometry(
-    side_length=SIDE_LENGTH,
-    compartment_ratio=COMPARTMENT_RATIO,
-    sphere_position_x=SPHERE_POSITION_X,
-    sphere_radius=SPHERE_RADIUS,
-    mesh_size=MESH_SIZE
-)
+    # Add buffer species (non-diffusive)
+    buffer = simulation.add_species('buffer', valence=-1)
+    cube.add_diffusion(buffer, 0 * u.um**2 / u.s)
+    if sensor_left:
+        cube.initialize_species(buffer, {'left': 0 * mM, 'right': buffer_init, 'sphere': 0 * mM})
+    else:
+        cube.initialize_species(buffer, {'left': 0 * mM, 'right': buffer_init, 'sphere': buffer_init})
 
-# Create simulation
-simulation = ecsim.Simulation('sensor', mesh, result_root='results')
+    # Add buffer complex species (non-diffusive)
+    ca_buffer = simulation.add_species('ca_buffer', valence=0)
+    cube.initialize_species(ca_buffer, 0 * mM)
+    cube.add_diffusion(ca_buffer, 0 * u.um**2 / u.s)
 
-# Compartments
-cube = simulation.simulation_geometry.compartments['cube']
+    # Add reversible binding reaction: Ca + buffer ↔ complex
+    cube.add_reaction(reactants=[ca, buffer], products=[ca_buffer], k_f=kf_b, k_r=kr_b)
 
-# Add species - Ca
-ca = simulation.add_species("ca", valence=2)
-cube.initialize_species(ca, CA_INIT)
-cube.add_diffusion(ca, 600 * u.um**2 / u.s)
+    # Add sensor species (non-diffusive)
+    sensor = simulation.add_species('sensor', valence=-1)
+    cube.add_diffusion(sensor, 0 * u.um**2 / u.s)
+    cube.initialize_species(sensor, {'left': 0 * mM, 'right': 0 * mM, 'sphere': sensor_init})
 
-# Add buffer species (non-diffusive)
-buffer = simulation.add_species('buffer', valence=-1)
-cube.add_diffusion(buffer, 0 * u.um**2 / u.s)
-if SENSOR_LEFT:
-    cube.initialize_species(buffer, {'left': 0 * mM, 'right': BUFFER_INIT, 'sphere': 0 * mM})
-else:
-    cube.initialize_species(buffer, {'left': 0 * mM, 'right': BUFFER_INIT, 'sphere': BUFFER_INIT})
+    # Add sensor complex species (non-diffusive)
+    ca_sensor = simulation.add_species('ca_sensor', valence=0)
+    cube.initialize_species(ca_sensor, 0 * mM)
+    cube.add_diffusion(ca_sensor, 0 * u.um**2 / u.s)
 
-# Add buffer complex species (non-diffusive)
-ca_buffer = simulation.add_species('ca_buffer', valence=0)
-cube.initialize_species(ca_buffer, 0 * mM)
-cube.add_diffusion(ca_buffer, 0 * u.um**2 / u.s)
+    if sensor_active:
+        # Add reversible binding reaction: Ca + sensor ↔ complex
+        cube.add_reaction(reactants=[ca, sensor], products=[ca_sensor], k_f=kf_s, k_r=kr_s)
 
-# Add reversible binding reaction: Ca + buffer ↔ complex
-cube.add_reaction(reactants=[ca, buffer], products=[ca_buffer], k_f=KF_B, k_r=KR_B)
-
-# Add sensor species (non-diffusive)
-sensor = simulation.add_species('sensor', valence=-1)
-cube.add_diffusion(sensor, 0 * u.um**2 / u.s)
-cube.initialize_species(sensor, {'left': 0 * mM, 'right': 0 * mM, 'sphere': SENSOR_INIT})
-
-# Add sensor complex species (non-diffusive)
-ca_sensor = simulation.add_species('ca_sensor', valence=0)
-cube.initialize_species(ca_sensor, 0 * mM)
-cube.add_diffusion(ca_sensor, 0 * u.um**2 / u.s)
-
-if SENSOR_ACTIVE:
-    # Add reversible binding reaction: Ca + sensor ↔ complex
-    cube.add_reaction(reactants=[ca, sensor], products=[ca_sensor], k_f=KF_S, k_r=KR_S)
+    # Simulate
+    simulation.run(end_time=1 * u.s, time_step=1 * u.ms, record_interval=100 * u.ms)
 
 
-### Simulate
-simulation.run(end_time=1 * u.s, time_step=1 * u.ms, record_interval=10 * u.ms)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the sensor simulation.")
+    parser.add_argument(
+        "--buffer_kd",
+        type=float,
+        default=400,
+        help="Buffer dissociation constant in nM.",
+    )
+    parser.add_argument(
+        "--sensor_kd",
+        type=float,
+        default=420,
+        help="Sensor dissociation constant in nM.",
+    )
+
+    args = parser.parse_args()
+
+    # Convert command-line arguments to appropriate units
+    run_sensor_simulation(
+        buffer_kd=args.buffer_kd * nM,
+        sensor_kd=args.sensor_kd * nM,
+    )
