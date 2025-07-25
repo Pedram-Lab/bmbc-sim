13a13523b6f6400249b9cbfd21e490ccdc081912
nprzrosas <nprzrosas@gmail.com>
2025-07-25 17:06:14 -0400

    Improve plots

--- scripts/electrostatics_visualization.py
+++ scripts/electrostatics_visualization.py
@@ -72,7 +72,6 @@ region_sizes = result_loader.compute_region_sizes()
 # Plot 1: Free and total Ca
 plt.rcParams['lines.linewidth'] = 2
 fig, axes = plt.subplots(1, 2, figsize=(fig_width, fig_height), sharex=True)
-#fig, axes = plt.subplots(1, 2, figsize=(18, 6), sharex=True)
 for ax, region in zip(axes, regions):
     region_size = region_sizes[region]
     ax.plot(
@@ -102,7 +101,6 @@ plt.close()
 
 # Plot 2: Mobile buffer
 fig2, axes = plt.subplots(1, 2, figsize=(fig_width, fig_height), sharex=True)
-#fig2, axes = plt.subplots(1, 2, figsize=(18, 6), sharex=True)
 for ax2, region in zip(axes, regions):
     region_size = region_sizes[region]
     ax2.plot(
@@ -127,7 +125,6 @@ plt.close()
 
 # Plot 3: Immobile buffer
 fig3, axes = plt.subplots(1, 2, figsize=(fig_width, fig_height), sharex=True)
-#fig3, axes = plt.subplots(1, 2, figsize=(18, 6), sharex=True)
 for ax3, region in zip(axes, regions):
     region_size = region_sizes[region]
     ax3.plot(
@@ -154,7 +151,6 @@ plt.close()
 
 # Plot 4: All species
 fig4, axes = plt.subplots(1, 2, figsize=(fig_width, fig_height), sharex=True)
-#fig4, axes = plt.subplots(1, 2, figsize=(18, 6), sharex=True)
 for ax4, region in zip(axes, regions):
     region_size = region_sizes[region]
     ax4.plot(
@@ -199,7 +195,6 @@ plt.close()
 # Plot 5: Potential
 plt.rcParams['lines.linewidth'] = 2
 fig5, axes = plt.subplots(1, 2, figsize=(fig_width, fig_height), sharex=True)
-#fig5, axes = plt.subplots(1, 2, figsize=(18, 6), sharex=True)
 for ax5, region in zip(axes, regions):
     region_size = region_sizes[region]
     ax5.plot(
@@ -215,7 +210,7 @@ for ax5, region in zip(axes, regions):
 axes[0].legend()
 plt.subplots_adjust(wspace=0.3)
 plt.suptitle("Electrostatics simulation", fontsize=9)
-#plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 plt.savefig(f"{file_prefix}_potential.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
