776e6b3b09171a510b6d2f53cb9b9c82d8e19190
Michael Innerberger <innerbergerm@hhmi.org>
2025-09-22 12:07:16 -0400

    Adjust concentration for volume change

--- scripts/tutorials/hydrogel_swelling_dynamic.py
+++ scripts/tutorials/hydrogel_swelling_dynamic.py
@@ -66,6 +66,13 @@ diffusion_stiffness += ALPHA * ngs.grad(psi) * ngs.grad(phi) * ngs.dx
 diffusion_mass = ngs.BilinearForm(diffusion_fes)
 diffusion_mass += psi * phi * ngs.dx
 
+patch_mass = ngs.LinearForm(diffusion_fes)
+patch_mass += psi * ngs.dx
+patch_mass.Assemble()
+prev_mass = patch_mass.vec.CreateVector()
+curr_mass = patch_mass.vec.CreateVector()
+prev_mass.data = patch_mass.vec
+
 concentration.Set(ngs.exp(-10 * ngs.x * ngs.x))
 diffusion_res = concentration.vec.CreateVector()
 
@@ -83,6 +90,13 @@ for step in range(N_STEPS):
         inv = elasticity_stiffness.mat.Inverse(elasticity_fes.FreeDofs())
         deformation.vec.data -= inv * elasticity_res
     deformation_history.AddMultiDimComponent(deformation.vec)
+    mesh.SetDeformation(deformation)
+
+    # Adjust previous concentration to new volume
+    patch_mass.Assemble()
+    curr_mass.data = patch_mass.vec
+    concentration.vec.FV().NumPy()[:] *=  prev_mass.FV().NumPy() / curr_mass.FV().NumPy()
+    prev_mass.data = curr_mass
 
     # Solve diffusion problem
     diffusion_stiffness.Assemble()
@@ -94,9 +108,12 @@ for step in range(N_STEPS):
     concentration.vec.data += m_star_inv * diffusion_res
     concentration_history.AddMultiDimComponent(concentration.vec)
 
-settings = dict(deformation=1)
+
+mesh.UnsetDeformation()
 animation_settings = dict(interpolate_multidim=True, animate=True)
-Draw(deformation_history, mesh, "displacement", settings=settings, **animation_settings)
-Draw(concentration_history, mesh, **animation_settings)
+el_settings = dict(min=-0.2, max=0.1, autoscale=False)
+diff_settings = dict(min=0, max=1.5, autoscale=False)
+Draw(deformation_history, mesh, settings=dict(deformation=1), **animation_settings, **el_settings)
+Draw(concentration_history, mesh, settings=diff_settings, **animation_settings, **diff_settings)
 
 # %%
