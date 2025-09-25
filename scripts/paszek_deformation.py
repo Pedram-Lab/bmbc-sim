7cad8495132ad590751331c29b7289be15df06e2
Michael Innerberger <innerbergerm@hhmi.org>
2025-09-25 13:45:26 -0400

    Clean up paszek deformation example and make it run again

--- scripts/paszek_deformation.py
+++ scripts/paszek_deformation.py
@@ -1,20 +1,7 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # Verify mechanical parameters based on the Paszek paper
-# This script recreates the setup of Paszek et al. *Integrin Clustering Is Driven by Mechanical Resistance from the Glycocalyx and the Substrate* (2009).
+# This script recreates the setup of Paszek et al. *Integrin Clustering Is
+# Driven by Mechanical Resistance from the Glycocalyx and the Substrate* (2009).
 #
 # According to this paper, Young's modulus is determined by the following expression:
 #
@@ -22,22 +9,22 @@
 #
 # where $\sigma$ is the Hookean spring constant, and $\Delta x$ is the LSM lattice node spacing.
 #
-# | Parameter                          | Definition                              | Value                      |
-# |------------------------------------|-----------------------------------------| ---------------------------|
-# | $\sigma_g$                         | Glycocalyx spring constant              | 0.01 pN/nm                 |
-# | $\sigma_m$                         | Membrane spring constant                | 0.4 pN/nm                  |
-# | $\sigma_b$                         | Bond spring constant                    | 2 pN/nm                    |
-# | $\sigma_s$                         | Substrate spring constant               | 1000 pN/nm                 |
-# | $\Delta x$                         | Lattice node spacing                    | 20 nm                      |
-# | $ν$                                | Poisson ratio                           | 0.25                       |
-# | Y$_g$                              | Young's modulus for glycocalyx          | 0.0025 pN/nm$^2$           |
-# | Y$_{m}$                            | Young's modulus for PM                  | 0.05 pN/nm$^2$             |
-# | Y$_{b}$                            | Young's modulus for bond                | 0.25 pN/nm$^2$             |
-# | Y$_{s}$                            | Young's modulus for substrate           | 125 pN/nm$^2$              |
-# | l$_{g}$                            | Glycocalyx thickness                    | 45 nm                      |
-# | l$_{b}$                            | Equilibrium bond length                 | 18 nm                      |
-# | F                                  | Bond force                              | 4 pN                       |
-# | l$_{d}$                            | Equilibrium separation distance         | 13.5 nm                    |
+# | Parameter   | Definition                      | Value            |
+# |-------------|---------------------------------| -----------------|
+# | $\sigma_g$  | Glycocalyx spring constant      | 0.01 pN/nm       |
+# | $\sigma_m$  | Membrane spring constant        | 0.4 pN/nm        |
+# | $\sigma_b$  | Bond spring constant            | 2 pN/nm          |
+# | $\sigma_s$  | Substrate spring constant       | 1000 pN/nm       |
+# | $\Delta x$  | Lattice node spacing            | 20 nm            |
+# | $ν$         | Poisson ratio                   | 0.25             |
+# | Y$_g$       | Young's modulus for glycocalyx  | 0.0025 pN/nm$^2$ |
+# | Y$_{m}$     | Young's modulus for PM          | 0.05 pN/nm$^2$   |
+# | Y$_{b}$     | Young's modulus for bond        | 0.25 pN/nm$^2$   |
+# | Y$_{s}$     | Young's modulus for substrate   | 125 pN/nm$^2$    |
+# | l$_{g}$     | Glycocalyx thickness            | 45 nm            |
+# | l$_{b}$     | Equilibrium bond length         | 18 nm            |
+# | F           | Bond force                      | 4 pN             |
+# | l$_{d}$     | Equilibrium separation distance | 13.5 nm          |
 #
 #
 # | Compartment                          | Size                         |
@@ -48,133 +35,162 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
-from netgen.csg import *
-from ngsolve import *
+import astropy.units as u
+import ngsolve as ngs
+import netgen.occ as occ
 from ngsolve.webgui import Draw
 
-from ecsim.units import *
+from ecsim.units import to_simulation_units
 
 # %%
 # Define units and parameters for the simulation
-s = convert(1.4 * u.um, LENGTH) / 2
-ecs_height = convert(45 * u.nm, LENGTH)
-membrane_height = convert(40 * u.nm, LENGTH)
-mesh_size = convert(10 * u.nm, LENGTH)
-cutout_size = convert(300 * u.nm, LENGTH)
+S = to_simulation_units(1.4 * u.um) / 2
+ECS_HEIGHT = to_simulation_units(45 * u.nm)
+MEMBRANE_HEIGHT = to_simulation_units(40 * u.nm)
+MESH_SIZE = to_simulation_units(10 * u.nm)
+CUTOUT_SIZE = to_simulation_units(300 * u.nm)
 
-pulling_force = convert(10 * u.pN, FORCE)
-youngs_modulus_ecs = convert(2.5 * u.fN / u.nm ** 2, PRESSURE)
-youngs_modulus_membrane = convert(50 * u.fN / u.nm ** 2, PRESSURE)
-poisson_ratio = 0.25
+PULLING_FORCE = to_simulation_units(10 * u.pN)
+YOUNG_MODULUS_ECS = to_simulation_units(2.5 * u.fN / u.nm ** 2)
+YOUNG_MODULUS_MEMBRANE = to_simulation_units(50 * u.fN / u.nm ** 2)
+POISSON_RATIO = 0.25
 
 # %%
 # Define geometry (we consider the substrate as fixed and don't model it explicitly)
-ecs = OrthoBrick(Pnt(-s, -s, 0), Pnt(s, s, ecs_height)).mat("ecs").bc("side")
-membrane = OrthoBrick(Pnt(-s, -s, ecs_height), Pnt(s, s, ecs_height + membrane_height)).mat("membrane").bc("side")
+ecs = occ.Box((-S, -S, 0), (S, S, ECS_HEIGHT)).mat("ecs").bc("side")
+membrane = occ.Box(
+    (-S, -S, ECS_HEIGHT),
+    (S, S, ECS_HEIGHT + MEMBRANE_HEIGHT)
+).mat("membrane").bc("side")
+geo = occ.Glue([ecs, membrane])
+geo.faces[4].bc("top")
+geo.faces[10].bc("interface")
+geo.faces[11].bc("substrate")
 
-geo = CSGeometry()
-geo.Add(ecs)
-geo.Add(membrane)
-
-mesh = Mesh(geo.GenerateMesh(maxh=mesh_size))
-mesh.ngmesh.SetBCName(0, "substrate")
-mesh.ngmesh.SetBCName(4, "top")
-mesh.ngmesh.SetBCName(9, "interface")
-# Draw(mesh)
+geo = occ.OCCGeometry(geo)
+mesh = ngs.Mesh(geo.GenerateMesh(maxh=MESH_SIZE))
+Draw(mesh)
 
 # %%
 # Set Lamé constants based on Young's modulus and Poisson ratio
-E = mesh.MaterialCF({"ecs": youngs_modulus_ecs, "membrane": youngs_modulus_membrane})
-nu = poisson_ratio
+E = mesh.MaterialCF({"ecs": YOUNG_MODULUS_ECS, "membrane": YOUNG_MODULUS_MEMBRANE})
+NU = POISSON_RATIO
 
-mu = E / (2 * (1 + nu))
-lam = E * nu / ((1 + nu) * (1 - 2 * nu))
+mu = E / (2 * (1 + NU))
+lam = E * NU / ((1 + NU) * (1 - 2 * NU))
 
 clipping = {"function": False,  "pnt": (0, 0, 0), "vec": (0, 1, 0)}
-visualization_settings = {"camera": {"transformations": [{"type": "rotateX", "angle": -80}]}, "deformation": 3.0}
+visualization_settings = {
+    "camera": {"transformations": [{"type": "rotateX", "angle": -80}]},
+    "deformation": 3.0,
+}
 
 
 # %%
 def stress(strain):
-    return 2 * mu * strain + lam * Trace(strain) * Id(3)
+    """Stress-strain relation for linear isotropic materials."""
+    return 2 * mu * strain + lam * ngs.Trace(strain) * ngs.Id(3)
 
 # %%
 # Define mechanic problem
-fes = VectorH1(mesh, order=1, dirichlet="substrate")
+fes = ngs.VectorH1(mesh, order=1, dirichlet="substrate")
 u, v = fes.TnT()
 
-blf = BilinearForm(fes)
-blf += InnerProduct(stress(Sym(Grad(u))), Sym(Grad(v))).Compile() * dx
+blf = ngs.BilinearForm(fes)
+blf += ngs.InnerProduct(stress(ngs.Sym(ngs.Grad(u))), ngs.Sym(ngs.Grad(v))).Compile() * ngs.dx
 blf.Assemble()
 a_inv = blf.mat.Inverse(fes.FreeDofs())
 
 
 # %%
 def compute_solution(points, force):
-    deformation = GridFunction(fes)
-    lf = LinearForm(fes)
+    """
+    Compute elastic deformation of membrane and ECS for given pulling forces
+    at specified points.
+    """
+    deformation = ngs.GridFunction(fes)
+    lf = ngs.LinearForm(fes)
     for p in points:
         lf += (-force * v[2])(*p)
-    
-    with TaskManager():
+
+    with ngs.TaskManager():
         lf.Assemble()
         deformation.vec.data = a_inv * lf.vec
     return deformation
 
 
 # %%
-def sample_cutout(mesh, deformation, n_samples=300):
-    x = np.linspace(-cutout_size / 2, cutout_size / 2, n_samples)
-    y = np.linspace(-cutout_size / 2, cutout_size / 2, n_samples)
-    X, Y = np.meshgrid(x, y)
-    z = ecs_height
+def sample_cutout(fe_mesh, deformation, n_samples=300):
+    """Sample deformation on a square cutout centered at (0, 0, ECS_HEIGHT)."""
+    x = np.linspace(-CUTOUT_SIZE / 2, CUTOUT_SIZE / 2, n_samples)
+    y = np.linspace(-CUTOUT_SIZE / 2, CUTOUT_SIZE / 2, n_samples)
+    x, y = np.meshgrid(x, y)
+    z = ECS_HEIGHT
 
-    points = [(x, y, z) for x, y in zip(X.flatten(), Y.flatten())]
-    mips = [mesh(*p) for p in points]
+    points = [(x, y, z) for x, y in zip(x.flatten(), y.flatten())]
+    mips = [fe_mesh(*p) for p in points]
     deformation_z = [deformation.components[2](p) for p in mips]
 
-    return X, Y, z + np.array(deformation_z).reshape((n_samples, n_samples))
+    return x, y, z + np.array(deformation_z).reshape((n_samples, n_samples))
 
 # %%
 # Compute elastic deformation of membrane and ECS for one ...
-points = [(0, 0, ecs_height)]
-deformation_1 = compute_solution(points, pulling_force)
+pulling_points = [(0, 0, ECS_HEIGHT)]
+deformation_1 = compute_solution(pulling_points, PULLING_FORCE)
 # Draw(deformation_1, settings=visualization_settings, clipping=clipping)
 
 # %%
 # ... two ...
-points = [(-0.04, 0, ecs_height), (0.04, 0, ecs_height)]
-deformation_2 = compute_solution(points, 0.9 * pulling_force)
+pulling_points = [(-0.04, 0, ECS_HEIGHT), (0.04, 0, ECS_HEIGHT)]
+deformation_2 = compute_solution(pulling_points, 0.9 * PULLING_FORCE)
 # Draw(deformation_2, settings=visualization_settings, clipping=clipping)
 
 # %%
 # ... and three points
-points = [(-0.03, -0.02, ecs_height), (0.03, -0.02, ecs_height), (0, 0.03, ecs_height)]
-deformation_3 = compute_solution(points, 0.7 * pulling_force)
+pulling_points = [(-0.03, -0.02, ECS_HEIGHT), (0.03, -0.02, ECS_HEIGHT), (0, 0.03, ECS_HEIGHT)]
+deformation_3 = compute_solution(pulling_points, 0.7 * PULLING_FORCE)
 # Draw(deformation_3, settings=visualization_settings, clipping=clipping)
 
 # %%
-def visualize_deformation_2d(mesh, deformation):
-    X, Y, Z = sample_cutout(mesh, deformation)
+def visualize_deformation_2d(fe_mesh, deformation):
+    """Visualize deformation as 2D heatmap."""
+    x, y, z = sample_cutout(fe_mesh, deformation)
     fig, ax = plt.subplots()
-    c = ax.pcolormesh(X, Y, (ecs_height - Z) * 1000, cmap="gnuplot", shading='gouraud', vmin=0, vmax=18)
+    c = ax.pcolormesh(
+        x,
+        y,
+        (ECS_HEIGHT - z) * 1000,
+        cmap="gnuplot",
+        shading="gouraud",
+        vmin=0,
+        vmax=18,
+    )
     ax.set_xlabel("x [µm]")
     ax.set_ylabel("y [µm]")
-    cbar = fig.colorbar(c, ax=ax, label='deformation [nm]')
+    fig.colorbar(c, ax=ax, label='deformation [nm]')
     plt.show()
 
 # %%
-def visualize_deformation_3d(mesh, deformation):
-    X, Y, Z = sample_cutout(mesh, deformation)
+def visualize_deformation_3d(fe_mesh, deformation):
+    """Visualize deformation as 3D surface plot."""
+    x, y, z = sample_cutout(fe_mesh, deformation)
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    c = ax.plot_surface(X, Y, (ecs_height - Z) * 1000, cmap="gnuplot", vmin=0, vmax=18, antialiased=False)
+    c = ax.plot_surface(
+        x,
+        y,
+        (ECS_HEIGHT - z) * 1000,
+        cmap="gnuplot",
+        vmin=0,
+        vmax=18,
+        antialiased=False,
+    )
     ax.set_xlim3d(-0.05, 0.05)
     ax.set_ylim3d(-0.05, 0.05)
     ax.view_init(160, 70, 0)
     ax.set_facecolor('black')
     ax.set_box_aspect((10, 10, 5))
     ax.set_axis_off()
-    cbar = fig.colorbar(c, ax=ax, label='deformation [nm]')
+    fig.colorbar(c, ax=ax, label='deformation [nm]')
     plt.show()
 
 # %%
@@ -188,5 +204,3 @@ visualize_deformation_2d(mesh, deformation_3)
 visualize_deformation_3d(mesh, deformation_1)
 visualize_deformation_3d(mesh, deformation_2)
 visualize_deformation_3d(mesh, deformation_3)
-
-# %%
